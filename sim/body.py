bc300afede53f816f7f476c09e8e34760e74823c
Lucien Gaitskell <lucieng@gaitskell.com>
2019-05-29 16:30:38 -0400

    Add new simulation code

---
+++ sim/body.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+class KEY:
+    POS = 0
+    VEL = 1
+    ACC = 2
+
+
+G = 6.67e-11  # m^3 / kg*s^2
+
+
+class Body:
+    def __init__(self, mass, pos, lock=False):
+        self.history = []
+        self.state = pos
+        self.mass = mass
+        self.LOCK = lock
+
+        self.curr_state = None
+
+    def accel_from_pos(self, b2, pos=None):
+        if self.LOCK: return np.array([0.,0.])
+        if pos is None:
+            pos = self.history[-1][0]
+
+        diff = pos - b2.state[0]
+        r = np.linalg.norm(diff)
+        d = -diff / r  # Normalized and inverted x/y direction vector
+        a = d * (b2.mass * G) / r ** 2  # Scale based on newton's gravitation equation
+        return a
+
+    @property
+    def state(self):
+        try:
+            return self.history[-1]
+        except IndexError:
+            return None
+
+    @state.setter
+    def state(self, state):
+        self.history.append(state)
---
+++ sim/simulation.py
@@ -0,0 +1,65 @@
+import math
+
+from .body import Body, KEY, np
+
+
+class Simulation:
+    def __init__(self, tstep, tmax, bodies):
+        # -- Simulation Vars --:
+        self.T_STEP = tstep  # sec
+        self.T_MAX = tmax  # sec
+        self.T_ITER = math.floor(tmax / tstep)  # number of iterations based on time designation
+
+        self.i_step = 0
+
+        self.bodies = bodies
+
+    def frame(self):
+        self.i_step += 1
+        for b in self.bodies:
+            b.curr_state = b.state
+
+        for i in range(0,5):
+            self._calc_accel()
+            self._move_frame()
+
+        for b in self.bodies:
+            b.history.append(None)
+            b.history[self.i_step] = b.curr_state
+
+    def _calc_accel(self):
+        for b in self.bodies:
+            b.curr_state[KEY.ACC] = np.array([0.,0.])
+            for t in self.bodies:
+                if b is t: continue
+                old_acc = b.accel_from_pos(t)
+                new_acc = b.accel_from_pos(t, pos=b.curr_state[KEY.POS])
+                b.curr_state[KEY.ACC] += (old_acc + new_acc)/2
+
+    def _move_frame(self):
+        t = self.T_STEP * self.i_step  # (sec) Time at step
+
+        for b in self.bodies:
+            self._body_calc_frame(b)
+
+    def flush_frame(self):
+        for b in self.bodies:
+            b.state = b.curr_state
+            b.curr_state = None
+
+    def _body_calc_frame(self, b):
+        last = b.history[self.i_step - 1]
+
+        ap = b.curr_state[KEY.ACC]
+        vp = last[KEY.VEL] + ap * self.T_STEP
+        xp = last[KEY.POS] + (vp) * self.T_STEP
+
+        rad = np.linalg.norm(xp)
+        vp_speed = np.linalg.norm(vp)
+        ang_momentum = rad * vp_speed
+
+        if len(b.history) > self.i_step+1:
+            raise IndexError("Too many history elements")
+        b.curr_state[KEY.POS] = xp
+        b.curr_state[KEY.VEL] = vp
+
