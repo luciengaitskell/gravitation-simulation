ea7e324a892b15630d76a3d9c8c6655f9c0d3227
Lucien Gaitskell <lucieng@gaitskell.com>
2019-05-31 09:27:28 -0400

    Add adaptive bodies list to graphics

--- orbit_vispy.py
+++ orbit_vispy.py
@@ -12,7 +12,6 @@ R_EARTH = 6.5e6  # m
 M_SUN = 1.989e30  # kg
 
 
-g = OrbitalGraphics()
 b = [
     Body(5, np.array([[50, 0.],[0., 10.], [0., 0.]])),
     #Body(M_EARTH, np.array([[ R_EARTH + 408.*1e3 , 0.],[0., 7660.], [0., 0.]])),
@@ -20,6 +19,7 @@ b = [
 Body(5e13, np.array([[0., 0.],[0., 0.], [0., 0.]]), lock=True)
 ]
 s = Simulation(np.array(0.1), np.array(95*60*4), b)
+g = OrbitalGraphics(b)
 
 if __name__ == '__main__' and sys.flags.interactive == 0:
     MAX = 10000
@@ -30,8 +30,9 @@ if __name__ == '__main__' and sys.flags.interactive == 0:
         tracking[i, 1] = b[0].history[i-1][KEY.POS, 1]
         tracking[i, 2] = 0
         if i % 3 == 0:
-            g.earth_transform = STTransform(translate=[b[0].state[0, 0], b[0].state[0, 1], 0])
+            g.bodies[0].transform = STTransform(translate=[b[0].state[0, 0], b[0].state[0, 1], 0])
             g.trails.set_data(tracking[0:i+1], edge_color=None, face_color=(1, 1, 1, .5), size=5)
-            g.sun.transform = STTransform(translate=[b[1].state[0, 0], b[1].state[0, 1], 0])
+            g.bodies[1].transform = STTransform(translate=[b[1].state[0, 0], b[1].state[0, 1], 0])
+            g.bodies[2].transform = STTransform(translate=[b[2].state[0, 0], b[2].state[0, 1], 0])
             print(np.linalg.norm(b[0].state[KEY.VEL]), b[0].state[KEY.ACC])
             g.draw()
--- visual/graphics.py
+++ visual/graphics.py
@@ -1,11 +1,12 @@
+import math
 import numpy as np
+
 from vispy import scene
 from vispy.visuals.transforms import STTransform
 
 
 class OrbitalGraphics:
     def __setup_objects(self):
-        self.sun.transform = STTransform(translate=[0, 0, 0])
         self.trails.set_data(np.array([[0, 0, 0], [0, 0, 0]]), edge_color=None, face_color=(1, 1, 1, .5), size=5)
 
     def __setup_camera(self):
@@ -13,18 +14,18 @@ class OrbitalGraphics:
         self.view.camera.elevation = 90
         self.view.camera.azimuth = 0
 
-    def __init__(self):
+    def __init__(self, bodies):
         self.canvas = scene.SceneCanvas(keys='interactive', bgcolor='black',
                                         size=(800, 600), show=True)
 
         self.view = self.canvas.central_widget.add_view()
         self.view.camera = 'turntable'
 
-        self.sun = scene.visuals.Sphere(radius=20, method='latitude', parent=self.view.scene,
-                                        edge_color='white')
+        self.bodies = []
 
-        self.earth = scene.visuals.Sphere(radius=1, method='latitude', parent=self.view.scene,
-                                          edge_color='white')
+        for b in bodies:
+            self.bodies.append(scene.visuals.Sphere(radius=math.pow(b.mass/1e12, 1/3), method='latitude',
+                                                    parent=self.view.scene, edge_color='white'))
 
         self.trails = scene.visuals.Markers()
         self.view.add(self.trails)
@@ -32,13 +33,14 @@ class OrbitalGraphics:
         self.__setup_objects()
         self.__setup_camera()
 
+    '''
     @property
     def earth_transform(self):
         return self.earth.transform
 
     @earth_transform.setter
     def earth_transform(self, trans):
-        self.earth.transform = trans
+        self.earth.transform = trans'''
 
     def draw(self, t=0.1):
         im = self.canvas.render()
